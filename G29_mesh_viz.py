55d8cf0ed14b2977a22ef6c37ec0e3cc6ce5354e
Luis Medina <luisdamed@gmail.com>
2022-08-01 12:24:34 +0200

    First commit

---
+++ G29_mesh_viz.py
@@ -0,0 +1,44 @@
+#%%
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+
+# Read data from a csv
+z_data = pd.read_csv('test_ender.csv', delimiter= " ", skiprows = 0, skipinitialspace= "true"  )
+
+# Get current time
+now = datetime.now()
+current_time = now.strftime("%d/%b/%y %H:%M:%S")
+title_string = f'G29 Mesh data - {current_time}'
+
+# Create surface figure
+fig = go.Figure(data=[go.Surface(z=z_data.values)])
+
+
+#%% Formatting
+fig.update_layout(title= title_string, title_y = .9, autosize=False,
+                  width=700, height=450,
+                  margin=dict(l=50, r=10, b=10, t=20))
+
+# Use per-axis property definition
+fig.update_scenes(xaxis_range = [0, len(z_data.columns.values) ],
+                  yaxis_range = [0, len(z_data.index.values) ],
+                  zaxis_range=[-1, 1]  )
+
+# Or pack the properties in dict format (more compact and clean)                                 
+fig.update_scenes( aspectratio = {'x': .7, 'y': .7, 'z' : 0.5},
+                    camera_eye = {'x': 0, 'y': -1, 'z' : 0.9},
+                    camera_projection_type='perspective')
+
+fig.update_traces(colorscale = 'Portland') #RdBu
+fig.update_layout(plot_bgcolor='white')
+fig.update_coloraxes(colorbar_lenmode='pixels', colorbar_len= 200)
+fig.show()
+
+# Export to html
+fig.write_html("test_mesh.html")
+
+
